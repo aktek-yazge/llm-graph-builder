adc2682e35b92a3031340398c04a0601d4dac272
Mehmet Erdoğan <mehmet.erdogan@aktekbilisim.com>
2025-07-29 00:09:59 +0000

    Chunks oluşturma işlemini geçici olarak devre dışı bırak ve sayfaları doğrudan chunks olarak ata

--- backend/src/main.py
+++ backend/src/main.py
@@ -1054,9 +1054,11 @@ def get_chunkId_chunkDoc_list(
         print("pages2:", pages)
         create_chunks_obj = CreateChunksofDocument(pages, graph)
         print("create_chunks_obj:", create_chunks_obj)
-        chunks = create_chunks_obj.split_file_into_chunks(
-            token_chunk_size, chunk_overlap
-        )
+        # chunks = create_chunks_obj.split_file_into_chunks(
+        #     token_chunk_size, chunk_overlap
+        # )
+
+        chunks = pages
         print("chunks: ", chunks)
         chunkId_chunkDoc_list = create_relation_between_chunks(graph, file_name, chunks)
         return len(chunks), chunkId_chunkDoc_list
